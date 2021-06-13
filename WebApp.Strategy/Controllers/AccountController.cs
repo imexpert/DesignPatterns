838397d962dc44d0cfa66d396c2b009174b106f8
ali.unal <aosmanunal@gmail.com>
2021-06-13 18:21:17 +0300

    Strategy Design Pattern Project Oluşturuldu

---
+++ WebApp.Strategy/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Strategy.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if (hasUser == null)
+            {
+                return View();
+            }
+
+            var siginResult = await _signInManager.PasswordSignInAsync(hasUser, password, true, false);
+
+            if (!siginResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ WebApp.Strategy/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp.Strategy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+                identityDbContext.Database.Migrate();
+
+                if (!userManager.Users.Any())
+                {
+                    userManager.CreateAsync(new AppUser()
+                    {
+                        UserName = "user1",
+                        Email = "aosmanunal1@gmail.com"
+                    }, "Password123*").Wait();
+
+                    userManager.CreateAsync(new AppUser()
+                    {
+                        UserName = "user2",
+                        Email = "aosmanunal2@gmail.com"
+                    }, "Password123*").Wait();
+
+                    userManager.CreateAsync(new AppUser()
+                    {
+                        UserName = "user3",
+                        Email = "aosmanunal3@gmail.com"
+                    }, "Password123*").Wait();
+
+                    userManager.CreateAsync(new AppUser()
+                    {
+                        UserName = "user4",
+                        Email = "aosmanunal4@gmail.com"
+                    }, "Password123*").Wait();
+
+                    userManager.CreateAsync(new AppUser()
+                    {
+                        UserName = "user5",
+                        Email = "aosmanunal5@gmail.com"
+                    }, "Password123*").Wait();
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
